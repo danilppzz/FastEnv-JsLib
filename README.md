bb3b46270fa38975827a19ba0bb97df1a2048592
d4niz <josedanielmorebolopez@gmail.com>
2023-10-13 00:23:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Fast Env is a simple library for loading and accessing environment variables from a `.env` file. 
+It is ideal for Node.js projects that require an easy way to manage environment configurations.
+
+## Features
+
+- Loads environment variables from a `.env` file in the project's root.
+- Provides a convenient way to access the loaded environment variables.
+
+## Installation
+
+To start using Fast Env in your project, follow these steps:
+
+Npm
+```
+npm install fenv
+```
+
+## How to use
+
+```js
+const Fenv = require('fenv');
+
+const env = new Fenv({ envFilePath: '/root/.env' });
+env.load();
+
+console.log(env.get('DATABASE_URL'));
+```
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+class Env {
+  constructor() {
+    this.envFilePath = path.resolve(process.cwd(), '.env');
+    this.config = {};
+    this.load();
+  }
+
+  load() {
+    if (fs.existsSync(this.envFilePath)) {
+      const envFile = fs.readFileSync(this.envFilePath, 'utf8');
+      const envLines = envFile.split('\n');
+      envLines.forEach(line => {
+        const [key, value] = line.split('=');
+        if (key && value) {
+          this.config[key] = value;
+        }
+      });
+    } else {
+      throw new Error(`Environment file ${this.envFilePath} not found.`);
+    }
+  }
+
+  get(key) {
+    return this.config[key];
+  }
+}
+
+module.exports = Env;
